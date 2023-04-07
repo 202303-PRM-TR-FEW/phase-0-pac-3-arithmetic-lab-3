6f6c89814fccb0d8b806f4768e94ba6ab3fbb55f
ali taslimpoor <shayannniiii@gmail.com>
2023-04-07 15:58:28 +0300

    done

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a , b){
+return a + b;
+}
+
+function subtract(a , b){
+return a - b;
+}
+         
+function multiply(a , b){
+return a * b;
+}
+
+function divide(a , b){
+return a / b;
+}
+
+function increment(n){
+    n+=1 ;
+    return n
+}
+
+function decrement(n){
+    n-=1;
+    return n
+}
+
+function makeInt(n){
+  n = parseInt(n , 10)
+    return n
+}
+function preserveDecimal(n){
+    n = parseFloat(n , 10)
+return n
+}
